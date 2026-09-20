d9cf79b54b7e453fdb477c4d30738a6a4b8f18ad
MahmoudMagdy001 <mahmodmansour2001@gmail.com>
2026-09-20 15:30:07 +0300

    fix(about): remove unused Link import to pass TypeScript strict build

--- src/features/about/pages/AboutPage.tsx
+++ src/features/about/pages/AboutPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Link } from 'react-router-dom'
 import { Sparkles } from 'lucide-react'
 import { CORE_VALUES } from '../../../data/initialData'
 import { MasterCtaSection } from '../../../components/home/MasterCtaSection'
